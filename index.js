83e3345941595a1a402bd6775c4de582599af088
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-10-24 14:36:37 +0100

    refactoring resolvers & typeDefs

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
-
+const typeDefs = require('./graphQl/typeDefs')
 const Post = require('./Models/Post')
 const { MONGO_DB } = require('./confg.js')
 
-const typeDefs = gql`
-  type Post{
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query{
-    getPosts: [Post]
-  }
-`
+
 
 const resolvers = {
   Query:{
