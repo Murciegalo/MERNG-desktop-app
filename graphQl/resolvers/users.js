ffd9df639fdd7f6bd93d9e85230a090b682026e6
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-10-26 15:10:26 +0100

    graphQL login completed

--- graphQl/resolvers/users.js
+++ graphQl/resolvers/users.js
@@ -4,7 +4,7 @@ const {UserInputError} = require('apollo-server');
 
 const User = require('../../Models/User');
 const {SECRET_KEY} = require('../../confg');
-const validateRegisterInput = require('../../utils/validators.js')
+const {validateRegisterInput,validateLoginInput} = require('../../utils/validators.js')
 
 module.exports = {
   Mutation: {
@@ -47,6 +47,35 @@ module.exports = {
         id: res._id,
         token
       }
+    },
+    async login(_,{email, password}){
+      const{ errors , valid } = validateLoginInput(email, password);
+      if(!valid){
+        throw new UserInputError('Errors' , {errors})
+      }
+      const user = await User.findOne({email})
+      if(!user){
+        errors.general = 'User not found'
+        throw new UserInputError('User not found', {errors})  
+      }
+      const match = await bcrypt.compare(password , user.password)
+      if(!match){
+        errors.general = 'Wrong Credentials'
+        throw new UserInputError('Wrong Credentials', {errors})
+      }
+      const token = jwt.sign({
+        id: user.id,
+        email: user.email,
+        username: user.username
+      }, 
+        SECRET_KEY , 
+        {expiresIn: '1h'}
+      );
+      return {
+        ...user._doc,
+        id: user._id,
+        token
+      }    
     } 
   }
 }
--- utils/validators.js
+++ utils/validators.js
@@ -24,4 +24,19 @@ const validateRegisterInput = (
   }
 }
 
-module.exports = validateRegisterInput;
+const validateLoginInput = ( email, password) => {
+  const errors = {};
+  const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+  if(email.trim() === '' || !email.match(regEx)){
+    errors.em = 'Please make sure your email is allright'
+  }
+  if(password === ''){
+    errors.password = 'Please check your password'
+  }
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1
+  }
+}
+
+module.exports = {validateRegisterInput ,validateLoginInput }
