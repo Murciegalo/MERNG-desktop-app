3ee5717dd1a41668a02ca90514be9bc0aafa7d5d
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-11-03 14:24:28 +0100

    Fetching data on home with graphQL set + UI completed

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import {Card, Image, Button} from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
+import {Card, Image, Button, Icon, Label} from 'semantic-ui-react'
 
 const PostCard = ({post:{ body, createdAt, id, username, likeCount, commentCount, likes }}) => {
+  const likePost = () => {
+    console.log('text')
+  }
+  const commentPost = () => {
+    console.log('tatat')
+  }
   return (
     <Card>
       <Card.Content>
@@ -12,9 +19,33 @@ const PostCard = ({post:{ body, createdAt, id, username, likeCount, commentCount
         />
         <Card.Header>{username}</Card.Header>
         <Card.Meta>{createdAt}</Card.Meta>
-        <Card.Description>{body}</Card.Description>
+        <Card.Description as={Link} to={`/posts/${id}`}>{body}</Card.Description>
       </Card.Content>
-      <Card.Content extra>hahaaha
+      <Card.Content extra>
+        <Button 
+          as="div" 
+          labelPosition="right"
+          onClick={likePost}
+        >
+          <Button color="teal" basic>
+            <Icon name="heart" />
+          </Button>
+          <Label basic color="teal" pointing="left">
+            {likeCount}
+          </Label>
+        </Button>
+        <Button 
+          as="div" 
+          labelPosition="right"
+          onClick={commentPost}
+        >
+          <Button color="blue" basic>
+            <Icon name="comments" />
+          </Button>
+          <Label basic color="blue" pointing="left">
+            {commentCount}
+          </Label>
+        </Button>
       </Card.Content>
     </Card>
   )
