d0028ff89a8ab04dffcf3568b51fda673e11fcc3
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-11-03 12:50:43 +0100

    login/register/home pages set. ReactRouter in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-
+import {BrowserRouter as Router , Route} from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css'
+import './App.css'
+import { Home } from './components/pages/Home';
+import { Login } from './components/pages/Login';
+import { Register } from './components/pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <Router>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Router>
   );
 }
 
