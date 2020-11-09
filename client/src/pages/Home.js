761181230164a2ba98017fce2224b88e9a9d4f71
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-11-09 14:36:55 +0100

    small bug at Home conditional rendering solved

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,21 +5,22 @@ import { Grid } from 'semantic-ui-react'
 import PostCard from '../components/PostCard'
 
 const Home = () => {
-  const { loading, data: {getPosts: posts }} = useQuery(FETCH_POSTS_QUERY)
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY)
+  const {getPosts} = data;
+  console.log(loading);
+  console.log(getPosts);
   return <Grid columns={3} divided>
     <Grid.Row className="page-title">
       <h1>Recent Posts</h1>
     </Grid.Row>
     <Grid.Row>
-      { loading && loading !== undefined ? (
-          <h1>Loading Posts...</h1> 
-        ) : (
-          posts && posts.map(post => (
+      { loading ? <h1>Loading Posts...</h1> :
+          getPosts.length > 0 ? ( getPosts.map(post => (
             <Grid.Column key={post.id} style={{marginBottom: 20}}>
               <PostCard post={post} />        
             </Grid.Column>
           ))
-        )
+        ): null
       }
     </Grid.Row>
   </Grid>
