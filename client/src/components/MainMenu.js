997a7a9ff9957fb105df46a8178677d0443d409e
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-11-09 16:40:20 +0100

    dinamic updates on menu after login/logout user

--- client/src/components/MainMenu.js
+++ client/src/components/MainMenu.js
@@ -1,14 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
+import {AuthContext} from '../context/auth'
 
 const MainMenu = () => {
+  const {user, logout} = useContext(AuthContext)
   const location = window.location.pathname;
   const path = location === '/' ? 'home' : location.substr(1) 
   const [activeItem, setActiveItem] = useState(path)
 
   const handleItemClick = (e, { name }) => setActiveItem(name)
-  return <Menu pointing secondary size="massive" color="teal">
+
+  const menuBar = user ? <Menu pointing secondary size="massive" color="teal">
+    <Menu.Item
+      name={user.username}
+      active
+      as={Link}
+      to="/"
+    />
+    <Menu.Menu position='right'>
+      <Menu.Item
+        name='logout'
+        onClick={logout}
+      />
+    </Menu.Menu>
+  </Menu> 
+  : 
+  <Menu pointing secondary size="massive" color="teal">
     <Menu.Item
       name='home'
       active={activeItem === 'home'}
@@ -33,5 +51,7 @@ const MainMenu = () => {
       />
     </Menu.Menu>
   </Menu>
+  
+  return menuBar;
 }
 export default MainMenu;
